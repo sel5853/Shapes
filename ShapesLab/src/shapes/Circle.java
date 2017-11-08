a34905d602ea8f5f45f3809c35bb9e7b0ecc6f96
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-11-08 16:12:25 -0500

    Added starter code and editted circle and rectangle

---
+++ ShapesLab/src/shapes/Circle.java
@@ -0,0 +1,38 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Circle implements Shape {
+
+	// fields
+	private int radius;
+	private static double pi = 3.14;
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param radius
+	 */
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double calculateArea() {
+		double area = (pi * radius * radius);
+		return area;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		double perimeter = (2 * pi * radius);
+		return perimeter;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle Radius: " + radius + "Circle Area: " + this.calculateArea() + "Circle Perimeter: " + this.calculatePerimeter();
+	}
+}
---
+++ ShapesLab/src/shapes/Rectangle.java
@@ -0,0 +1,32 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Rectangle implements Shape {
+
+	private int length;
+	private int width;
+
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+	}
+
+
+	public double calculateArea() {
+		return (length * width);
+	}
+
+
+	public double calculatePerimeter() {
+		return (2 * (length + width));
+	}
+
+
+	public String toString() {
+		return "Rectangle Width: " + width + " Length: " + length + " Area: " + this.calculateArea() + " Perimeter: "
+				+ this.calculatePerimeter();
+	}
+}
